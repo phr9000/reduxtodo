4a7daeddceb3323d3c755de995fa5e050825c535
phr9000 <phr9000@gmail.com>
2025-03-26 14:23:56 +0900

    normal todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "text1", checked: true },
+    { id: 2, text: "text2", checked: false },
+    { id: 3, text: "text3", checked: false },
+  ]);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const appContainer = {
+    textAlign: "center" as const,
+    margin: "0 auto",
+    width: "150px",
+    padding: "20px",
+  };
+
+  const writeDownContainer = {
+    display: "flex",
+  };
+
+  const listContainer = {
+    display: "flex",
+    flexDirection: "column" as const,
+    listStyle: "none",
+    padding: "0px",
+  };
+
+  // checkbox handle
+  const handleCheck = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
+  // 삭제 handle
+  const handleDelete = () => {
+    setTodos(todos.filter(todo => !todo.checked));
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  // 전송 handle
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (inputValue.trim() === "") return;
+    setTodos([...todos, { id: Date.now(), text: inputValue, checked: false }]);
+    setInputValue('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={appContainer}>
+        <form style={writeDownContainer} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="todo 사항을 입력해주세요"
+            onChange={handleChange}
+            value={inputValue}
+          />
+          <button
+            type="submit"
+            style={{ display: "block", minWidth: "fit-content" }}
+          >
+            전송
+          </button>
+        </form>
+        <ul style={listContainer}>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input type="checkbox" checked={todo.checked} onChange={()=> handleCheck(todo.id)} />
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+        <button type="button" onClick={handleDelete}>
+          선택된 항목 삭제
+        </button>
+      </div>
     </div>
   );
 }
