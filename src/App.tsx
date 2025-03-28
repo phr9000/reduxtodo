9594e33d6607d9d079125131b914a77ec6a1ae80
phr9000 <phr9000@gmail.com>
2025-03-28 16:23:25 +0900

    redux 중간커밋

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import CreateTodo from "./components/CreateTodo";
 import TodoList from "./components/TodoList";
 import RemoveBtn from "./components/RemoveBtn";
 import { useSelector, useDispatch } from "react-redux";
-import { createTodo, todoList, removeBtn } from "./slices/create";
+import { createTodo, toggleTodo, removeCheckedTodos } from "./slices/create";
 
 function App() {
+  const dispatch = useDispatch();
+
   const [todos, setTodos] = useState([
     { id: 1, text: "text1", checked: true },
     { id: 2, text: "text2", checked: false },
@@ -21,26 +23,29 @@ function App() {
   };
 
   const addTodo = (text: string) => {
-    console.log("text >>>>>>>>>", text);
+    // console.log("text >>>>>>>>>", text);
 
-    setTodos((prev) => [
-      ...prev,
-      { id: new Date().getTime(), text, checked: false },
-    ]);
+    // setTodos((prev) => [
+    //   ...prev,
+    //   { id: new Date().getTime(), text, checked: false },
+    // ]);
+    dispatch(createTodo(text));
   };
 
-  const onChangeCheck = (idx: number, checked: boolean) => {
-    setTodos((prevTodos) => {
-      const nextTods = [...prevTodos];
-      nextTods[idx].checked = checked;
-      return nextTods;
-    });
+  const onChangeCheck = (id: number, checked: boolean) => {
+    // setTodos((prevTodos) => {
+    //   const nextTods = [...prevTodos];
+    //   nextTods[idx].checked = checked;
+    //   return nextTods;
+    // });
+    dispatch(toggleTodo({id, checked}));
   };
 
   const onRemoveChecked = () => {
     // setTodos(todos.filter((todo) => !todo.checked));
     // 비동기 고려 아래 코드로 적용
-     setTodos((prevTodos) => prevTodos.filter((todo) => !todo.checked));
+    //  setTodos((prevTodos) => prevTodos.filter((todo) => !todo.checked));
+    dispatch(removeCheckedTodos());
   };
 
   return (
--- src/slices/create.ts
+++ src/slices/create.ts
@@ -8,28 +8,57 @@ interface TodoState {
 }
 
 // 초기 상태 설정
-const initialState: TodoState = {
-   id: 0, 
-   text: '',
-   checked: false
+const initialState: { todos: TodoState[] } = {
+   todos: [
+    { id: 1, text: "text1", checked: true },
+    { id: 2, text: "text2", checked: false },
+    { id: 3, text: "text3", checked: false },
+  ],
 };
 
-export const counterSlice = createSlice({
-  name: "create",
+export const todoSlice = createSlice({
+  name: "todo",
   initialState, // 초기 상태 적용
   reducers: {
-    createTodo: (state) => {
-      state.value += 1;
+    //     createTodo: (state) => {
+    //       state.value += 1;
+    //     },
+    //     todoList: (state) => {
+    //       state.value -= 1;
+    //     },
+    //     removeBtn: (state) => {
+    //       state.value += 1;
+    //     },
+    //   },
+    // 새 할 일 추가
+    createTodo: (state, action: PayloadAction<string>) => {
+      const newTodo: TodoState = {
+        id: new Date().getTime(),
+        text: action.payload,
+        checked: false,
+      };
+
+      state.todos.push(newTodo);
     },
-    todoList: (state) => {
-      state.value -= 1;
+
+    // 체크 상태 변경
+    toggleTodo: (
+      state,
+      action: PayloadAction<{ id: number; checked: boolean }>
+    ) => {
+      const todo = state.todos.find((t) => t.id === action.payload.id);
+      if (todo) {
+        todo.checked = action.payload.checked;
+      }
     },
-    removeBtn: (state) => {
-      state.value += 1;
+
+    // 체크된 할 일 삭제
+    removeCheckedTodos: (state) => {
+      state.todos = state.todos.filter((todo) => !todo.checked);
     },
   },
 });
 
-export const { createTodo, todoList, removeBtn } = counterSlice.actions;
+export const { createTodo, toggleTodo, removeCheckedTodos } = todoSlice.actions;
 
-export default counterSlice.reducer;
+export default todoSlice.reducer;
